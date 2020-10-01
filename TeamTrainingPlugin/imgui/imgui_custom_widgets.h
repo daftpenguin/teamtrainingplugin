1821ab6b4316456394ad8f9b7fdc477edf7cdb4f
Daft Penguin <daftpenguinrl@gmail.com>
2020-10-01 04:16:09 -0400

    Forgot to add custom imgui widgets for downloading/uploading progress

---
+++ TeamTrainingPlugin/imgui/imgui_custom_widgets.h
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include "imgui.h"
+#include "imgui_internal.h"
+
+using namespace std;
+
+namespace ImGui
+{
+    bool Spinner(const char* label, float radius, int thickness);
+}
